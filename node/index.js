97bbdc68d6873330e6644c0a0b9e47d5659408f5
William Chitto <william.chitto@mensa.org.br>
2025-04-28 18:19:39 -0400

    Update index.js

--- node/index.js
+++ node/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const axios = require('axios').default;
 const mysql = require('mysql');
 
 const app = express();
@@ -12,6 +11,19 @@ const config = {
   database: 'nodedb',
 };
 
+const names = [
+  'Luke Skywalker',
+  'Leia Organa',
+  'Han Solo',
+  'Darth Vader',
+  'Obi-Wan Kenobi',
+  'Yoda',
+  'Chewbacca',
+  'R2-D2',
+  'C-3PO',
+  'Boba Fett'
+];
+
 app.get('/', (req, res) => {
   insertPeopleName(res);
 });
@@ -20,39 +32,46 @@ app.listen(PORT, () => {
   console.log('STARTED AT ' + PORT);
 });
 
-async function getPersonName() {
-  const RANDOM = Math.floor(Math.random() * 10);
-  const response = await axios.get('https://swapi.dev/api/people');
-  personName = response.data.results;
-  return personName[RANDOM].name;
+function getPersonName() {
+  const randomIndex = Math.floor(Math.random() * names.length);
+  return names[randomIndex];
 }
 
-async function insertPeopleName(res) {
-  const name = await getPersonName();
+function insertPeopleName(res) {
+  const name = getPersonName();
   const connection = mysql.createConnection(config);
   const sql = `INSERT INTO people(name) values('${name}')`;
-    
-  connection.query(sql);
-  console.log(`${name} inserido no banco!`);    
-  getPeople(res, connection);
+  
+  connection.query(sql, (err) => {
+    if (err) {
+      console.error('Erro ao inserir no banco:', err);
+      res.status(500).send('Erro ao inserir no banco de dados.');
+      return;
+    }
+    console.log(`${name} inserido no banco!`);
+    getPeople(res, connection);
+  });
 }
 
-function getPeople(res, connection) {    
+function getPeople(res, connection) {
   const sql = `SELECT id, name FROM people`;  
   
   connection.query(sql, (error, results, fields) => {
     if (error) {
-      throw error
-    };
+      console.error('Erro ao buscar dados:', error);
+      res.status(500).send('Erro ao buscar dados.');
+      return;
+    }
     
-    let table = '<table>';
+    let table = '<table border="1">';
     table += '<tr><th>#</th><th>Name</th></tr>';
-    for(let people of results) {      
+    for (let people of results) {
       table += `<tr><td>${people.id}</td><td>${people.name}</td></tr>`;
     }
-
     table += '</table>';    
-    res.send('<h1>Full Cycle Rocks!</h1>' + table);    
-  });   
+    
+    res.send('<h1>Full Cycle Rocks!</h1>' + table);
+  });
+
   connection.end();
 }
